ba7293411077e2438313a30539b85c5cc36ed0ed
mutoe <imutoe@gmail.com>
2026-03-09 08:06:10 +0800

    feat: 启用 数据导出功能

--- packages/types/src/serverConfig.ts
+++ packages/types/src/serverConfig.ts
@@ -51,6 +51,8 @@ export interface GlobalServerConfig {
   defaultAgent?: PartialDeep<UserDefaultAgent>;
   disableEmailPassword?: boolean;
   enableBusinessFeatures?: boolean;
+  /** When true, show data export in Settings > Storage (can be enabled via ENABLE_DATA_EXPORT=1) */
+  enableDataExport?: boolean;
   /**
    * @deprecated
    */
